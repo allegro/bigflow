5fc764438b7a1c98886d9f2926e6c2906f56178b
Iwo Czerniawski <iwo.czerniawski@allegro.pl>
2019-08-23 16:03:15 +0200

    Add beam_manager

---
+++ biggerquery/beam_manager.py
@@ -0,0 +1,229 @@
+import avro.schema
+from apache_beam.io import BigQueryDisposition
+
+import avro.schema
+import apache_beam as beam
+from apache_beam.io.gcp.internal.clients import bigquery
+from apache_beam.options.pipeline_options import \
+    PipelineOptions, SetupOptions, StandardOptions, GoogleCloudOptions, WorkerOptions
+
+
+class BeamManager:
+    """
+    Used for read/write operations. It is base class for further operations.
+    """
+    def __init__(self):
+        self.pipeline = None
+
+    def read_from_big_query(self, dataset, project_id, standard_sql, query):
+        return beam.io.Read(beam.io.BigQuerySource(
+            dataset=dataset,
+            project=project_id,
+            use_standard_sql=standard_sql,
+            query=query
+        ))
+
+    def read_from_avro(self, file_source_path):
+        return beam.io.ReadFromAvro(file_source_path)
+
+    def write_to_avro(self, file_output_path, schema):
+        return beam.io.WriteToAvro(
+            file_output_path,
+            avro.schema.parse(schema),
+            num_shards=1,
+            shard_name_template='S_N',
+            file_name_suffix='.avro')
+
+
+class TemplatedBeamManager:
+    """
+    Decorator that resolves table names/all kind of variables.
+    """
+
+    def __init__(self, beam_manager, internal_tables, external_tables, run_datetime, extras):
+        self.beam_manager = beam_manager
+        self.extras = extras
+        self.internal_tables = internal_tables
+        self.external_tables = external_tables
+        self.run_datetime = run_datetime
+
+    def read_from_big_query(self, dataset, project_id, standard_sql, query):
+        query = query.format(**self._template_variables())
+        return self.beam_manager.read_from_big_query(dataset, project_id, standard_sql, query)
+
+    def read_from_avro(self, file_source_path):
+        return self.beam_manager.read_from_avro(file_source_path)
+
+    def write_to_avro(self, file_output_path, schema):
+        return self.beam_manager.write_to_avro(file_output_path, schema)
+
+    def write_truncate_to_big_query(self, table_name, schema):
+        table_name = self._legacy_table_name(self._template_variables()[table_name]) + '$' + self.run_datetime.replace('-', '')
+        return beam.io.WriteToBigQuery(
+            table_name,
+            schema=schema,
+            write_disposition=BigQueryDisposition.WRITE_TRUNCATE)
+
+    def _legacy_table_name(self, table_name):
+        spl = table_name.split('.')
+        return spl[0] + ':' + '.'.join(spl[1:len(spl)])
+
+    @property
+    def run_datetime(self):
+        return self.run_datetime
+
+    def _template_variables(self):
+        result = {}
+        result.update(self.internal_tables)
+        result.update(self.external_tables)
+        result['dt'] = self.run_datetime
+        result.update(self.extras)
+        return result
+
+
+class DataflowManager:
+    """
+    Used for I/O operations, pipeline creation in DataFlow. It is base class for further operations.
+    """
+    def __init__(self,
+                 project_id,
+                 beam_manager,
+                 dataset,
+                 dataflow_bucket,
+                 requirements_file_path,
+                 region,
+                 machine_type
+                 ):
+        self.project_id = project_id
+        self.beam_manager = beam_manager
+        self.dataset = dataset
+        self.dataflow_bucket = dataflow_bucket
+        self.requirements_file_path = requirements_file_path
+        self.region = region
+        self.machine_type = machine_type
+        self.standard_sql = True
+
+    def read_from_big_query(self, sql):
+        return self.beam_manager.read_from_big_query(
+            self.dataset,
+            self.project_id,
+            self.standard_sql,
+            sql
+        )
+
+    def read_from_avro(self, file_source_path):
+        return self.beam_manager.read_from_avro(file_source_path)
+
+    def write_to_avro(self, file_output_path, schema):
+        return self.beam_manager.write_to_avro(file_output_path, schema)
+
+    @property
+    def run_datetime(self):
+        return self.beam_manager.run_datetime
+
+    def write_truncate_to_big_query(self, table_name, schema):
+        bigquery.TableFieldSchema
+        fields = [bigquery.TableFieldSchema(name=row['name'], type=row['type'].upper(), mode=row['mode'].upper()) for row in schema]
+        schema = bigquery.TableSchema(fields=fields)
+        return self.beam_manager.write_truncate_to_big_query(table_name, schema)
+
+    def create_dataflow_pipeline(self,
+                                 table_prefix=None,
+                                 local_runner=None):
+        options = PipelineOptions()
+
+        if not local_runner:
+            google_cloud_options = options.view_as(GoogleCloudOptions)
+            google_cloud_options.project = self.project_id
+            google_cloud_options.job_name = self._job_name(self.run_datetime, table_prefix)
+            google_cloud_options.staging_location = 'gs://{dataflow_bucket}/beam_runner/staging'.format(
+                dataflow_bucket=self.dataflow_bucket)
+            google_cloud_options.temp_location = 'gs://{dataflow_bucket}/beam_runner/temp'.format(
+                dataflow_bucket=self.dataflow_bucket)
+            google_cloud_options.region = self.region
+            options.view_as(WorkerOptions).machine_type = self.machine_type
+
+            options.view_as(SetupOptions).requirements_file = self.requirements_file_path
+            options.view_as(StandardOptions).runner = 'DataflowRunner'
+        else:
+            options.view_as(StandardOptions).runner = 'DirectRunner'
+        self.beam_manager.beam_manager.pipeline = beam.Pipeline(options=options)
+        return self.beam_manager.beam_manager.pipeline
+
+    def _job_name(self, day, table_prefix=None):
+        if table_prefix:
+            import re
+            table_prefix_slug = re.sub(r'[^a-z0-9]+', '-', table_prefix).strip('-')
+            return 'stats-{day}-{prefix}'.format(day=day, prefix=("" if table_prefix_slug is None else table_prefix_slug))
+        else:
+            return 'stats-{day}'.format(day=day)
+
+
+def _throw_on_none(param, param_name):
+    if param is None:
+        raise ValueError("%s must be defined" % param_name)
+
+
+def create_dataflow_manager(
+        project_id,
+        runtime,
+        dataset_name,
+        dataflow_bucket,
+        internal_tables,
+        external_tables,
+        requirements_file_path,
+        region,
+        machine_type,
+        extras=None):
+    """
+    Dataflow manager factory.
+
+    :param project_id: string full project id
+    :param runtime: string representing partition being processed YYYY-MM-DD
+    :param dataset_name: string dataset name(not dataset id)
+    :param dataflow_bucket: string bucket used for dataflow processes deployment
+    :param internal_tables: list of dataset table names that are gonna be available during processing
+    :param external_tables: dict where key is table alias and value is full table ID
+    :param requirements_file_path: string path where requirements file is stored
+    :param region: string location of machine for eg. 'europe-west1'
+    :param machine_type string machine type. for eg. 'n1-highmem-8'
+    :param extras: dict with custom parameters that will be available inside templates
+    :return: DataflowManager
+    """
+    internal_tables = internal_tables or {}
+    external_tables = external_tables or {}
+    extras = extras or {}
+    _throw_on_none(project_id, "project_id")
+    _throw_on_none(dataset_name, "dataset_name")
+    _throw_on_none(dataflow_bucket, "dataflow_bucket")
+    _throw_on_none(runtime, "runtime")
+    _throw_on_none(requirements_file_path, "requirements_file_path")
+    _throw_on_none(region, "region")
+    _throw_on_none(machine_type, "machine_type")
+
+    beam_manager = BeamManager()
+    templated_dataflow_manager = TemplatedBeamManager(beam_manager, internal_tables, external_tables, runtime, extras)
+    return DataflowManager(project_id, templated_dataflow_manager, dataset_name, dataflow_bucket, requirements_file_path, region, machine_type)
+
+
+EXAMPLE_SAMPLE_SCHEMA = '''
+{"namespace": "example.avro",
+ "type": "record",
+ "name": "exampleSample",
+ "fields": [
+     {"name": "id", "type": "string", "mode": "required"},
+     {"name": "name", "type": "string", "mode": "required"}
+ ]
+}
+'''
+
+EXAMPLE_OUTPUT_SCHEMA = '''
+{"namespace": "example.avro",
+ "type": "record",
+ "name": "exampleSample",
+ "fields": [
+     {"name": "id", "type": "string", "mode": "required"},
+     {"name": "name", "type": "string", "mode": "required"}
+ ]
+}
+'''
---
+++ test/beam_test_case.py
@@ -0,0 +1,54 @@
+import os
+import tempfile
+from unittest import TestCase
+
+import avro.schema
+from avro.datafile import DataFileWriter
+from avro.io import DatumWriter
+from avro.datafile import DataFileReader
+from avro.io import DatumReader
+
+
+class BeamTestCase(TestCase):
+    def setUp(self):
+        self.files_to_delete = []
+
+    def cleanUp(self):
+        for f in self.files_to_delete:
+            os.remove(f)
+
+    def empty_file(self, file_name_prefix):
+        _, file_path = tempfile.mkstemp(prefix=file_name_prefix)
+        self.files_to_delete.append(file_path)
+        return file_path
+
+    def read_from_avro(self, avro_file_path):
+        return _read_from_avro(avro_file_path)
+
+    def create_avro_file(self, schema, items, file_prefix):
+        result_file_path =  _create_avro_file(schema, items, file_prefix)
+        self.files_to_delete.append(result_file_path)
+        return result_file_path
+
+
+def _read_from_avro(avro_file_path):
+    avro_file_path = _translate_to_sharded_avro_file_name(avro_file_path)
+    reader = DataFileReader(open(avro_file_path, 'rb'), DatumReader())
+    result = [s for s in reader]
+    reader.close()
+    return result
+
+
+def _create_avro_file(schema, items, file_prefix):
+    _, result_file_path = tempfile.mkstemp(prefix=file_prefix, suffix='.avro')
+    parsed_schema = avro.schema.parse(schema)
+    with open(result_file_path, 'wb') as f:
+        writer = DataFileWriter(f, DatumWriter(), parsed_schema)
+        for s in items:
+            writer.append(s)
+        writer.close()
+    return result_file_path
+
+
+def _translate_to_sharded_avro_file_name(file_name):
+    return file_name + '0_1.avro'
