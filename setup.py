3bec0e289bcf06782ab906790ca16bd89458ed91
Iwo Czerniawski <iwo.czerniawski@allegro.pl>
2020-02-27 15:18:15 +0100

    bump up apache-beam version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(os.path.join('requirements', 'stackdriver_extras.txt'), 'r') as stackd
 
 setuptools.setup(
     name="biggerquery",
-    version="0.6.dev30",
+    version="0.6.dev31",
     author=u"Chi",
     author_email="chibox-team@allegrogroup.com",
     description="BigQuery client wrapper with clean API",
