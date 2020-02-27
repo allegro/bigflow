58bc83f566f4e45f9f8046c7b2ef157f25943574
Iwo Czerniawski <iwo.czerniawski@allegro.pl>
2020-02-27 15:17:15 +0100

    bump up apache-beam version

--- e2e/config.py
+++ e2e/config.py
@@ -6,6 +6,6 @@ CREDENTIALS = None
 try:
     from . import local_config
     PROJECT_ID = local_config.PROJECT_ID
-    CREDENTIALS = local_config.CREDENTIALS
+    # CREDENTIALS = local_config.CREDENTIALS
 except ImportError:
     pass
