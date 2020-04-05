3b46b9bd2c5e3178f0b00603692f952870ae6b42
Daniel Faderski <daniel.faderski@allegro.pl>
2020-04-05 12:41:34 +0200

    Fix import config tests

--- test/test_cli/test_import_config.py
+++ test/test_cli/test_import_config.py
@@ -1,3 +1,4 @@
+from glob import glob
 from unittest import TestCase
 
 import os
@@ -6,16 +7,21 @@ from biggerquery.cli.config_utlis import import_config, BiggerQueryConfigNotFoun
 
 
 class ImportConfigTestCase(TestCase):
+    def setUp(self):
+        # we have to rely on python directory/files searching mechanism, because we don't know from which point the tests
+        # will be executed
+        self.configs_dir_location = glob(os.path.join(os.path.abspath('.'), '**/configs'), recursive=True)[0]
+
     def test_should_import_config_using_relative_path(self):
         # when
-        config = import_config('example_dev_configuration', './configs')
+        config = import_config('example_dev_configuration', '.')
 
         # then
         self.assertEqual(config.name, 'example_dev_configuration')
 
     def test_should_import_config_using_absolute_path(self):
         # when
-        config = import_config('example_dev_configuration', os.path.abspath('./configs'))
+        config = import_config('example_dev_configuration', self.configs_dir_location)
 
         # then
         self.assertEqual(config.name, 'example_dev_configuration')
@@ -23,7 +29,7 @@ class ImportConfigTestCase(TestCase):
     def test_should_throw_error_when_config_not_found(self):
         # expect
         with self.assertRaises(BiggerQueryConfigNotFound):
-            import_config('example_dev_configuration', './configs/another_configs')
+            import_config('example_dev_configuration', os.path.join(self.configs_dir_location + 'another_configs'))
 
         with self.assertRaises(BiggerQueryConfigNotFound):
             import_config('not_existing_configuration', '.')
