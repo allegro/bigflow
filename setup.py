d2c87703002abab14b7e853b62b3801490849199
Bartlomiej Beczkowski <bartlomiej.beczkowski@allegro.pl>
2020-06-24 10:37:37 +0200

    bumping version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(os.path.join('requirements', 'stackdriver_extras.txt'), 'r') as stackd
 
 setuptools.setup(
     name="biggerquery",
-    version="1.0.dev1",
+    version="1.0.dev6",
     author=u"Chi",
     author_email="chibox-team@allegrogroup.com",
     description="BigQuery client wrapper with clean API",
