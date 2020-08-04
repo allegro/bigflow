c6b05486a63f19111157a7f48df84b11ce3ef3f0
Bartosz Walacik <bartosz.walacik@allegro.pl>
2020-08-04 13:57:38 +0200

    load image tar

--- test/test_cli.py
+++ test/test_cli.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 
 from biggerquery.cli import *
+from biggerquery.cli import _decode_version_number_from_file_name
 
 
 class CliTestCase(TestCase):
@@ -249,3 +250,48 @@ class CliTestCase(TestCase):
 
     def assert_started_jobs(self, ids):
         self.assertEqual(ids, import_module("test_module.Unused1").started_jobs)
+
+
+    def test_should_decode_version_number_from_file_name(self):
+        # given
+        f_release = self._touch_file('image-0.1.0.tar')
+        f_dev = self._touch_file('image-0.3.0.dev-4b45b638.tar')
+
+        # expect
+        self.assertEqual(_decode_version_number_from_file_name(f_release), '0.1.0')
+        self.assertEqual(_decode_version_number_from_file_name(f_dev), '0.3.0.dev-4b45b638')
+
+        f_release.unlink()
+        f_dev.unlink()
+
+    def test_raise_error_when_given_file_is_not_tar(self):
+        # given
+        f = self._touch_file('image-0.1.0.zip')
+
+        with self.assertRaises(ValueError):
+            # when
+            _decode_version_number_from_file_name(f)
+
+        f.unlink()
+
+    def test_raise_error_when_given_file_name_has_wrong_pattern(self):
+        # given
+        f = self._touch_file('image.0.1.0.tar')
+
+        with self.assertRaises(ValueError):
+            # when
+            _decode_version_number_from_file_name(f)
+
+        f.unlink()
+
+    def test_raise_error_when_given_file_does_not_exists(self):
+        with self.assertRaises(ValueError):
+            # when
+            _decode_version_number_from_file_name(Path('/Users/image-0.1122123.0.tar'))
+
+    def _touch_file(self, file_name):
+        workdir = Path(os.path.dirname(__file__))
+        f = workdir / file_name
+        f.touch()
+        return f
+
