ee93bf25331f5c7259978bb9489e97f301b35926
Iwo Czerniawski <iwo.czerniawski@allegro.pl>
2020-08-10 09:24:22 +0200

    build cli

--- setup.py
+++ setup.py
@@ -14,8 +14,8 @@ with open(os.path.join('requirements', 'stackdriver_extras.txt'), 'r') as stackd
 
 
 setuptools.setup(
-    name="biggerquery",
-    version="1.0.dev11",
+    name="bigflow",
+    version="1.0.dev12",
     author=u"Chi",
     author_email="chibox-team@allegrogroup.com",
     description="BigQuery client wrapper with clean API",
