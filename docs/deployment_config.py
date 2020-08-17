64c89c85d74a3f1acbdeb1cf011ad72ce52a4766
Bartlomiej Beczkowski <beczkowb@gmail.com>
2020-08-17 08:26:58 +0200

    improving docs project

--- docs/deployment_config.py
+++ docs/deployment_config.py
@@ -1,6 +1,7 @@
 from bigflow import Config
 
-deployment_config = Config(name='dev',
-                           properties={
-                               'docker_repository': 'eu.gcr.io/docker_repository_project/my-project',
-                           })
+deployment_config = Config(
+    name='dev',
+    properties={
+        'docker_repository': 'eu.gcr.io/docker_repository_project/my-project',
+    })
