ed7a9df9ff8e34f43d9e37c370bdc064f2f48b4d
Iwo Czerniawski <iwo.czerniawski@allegro.pl>
2020-08-26 15:21:07 +0200

    cleaning dependencies

--- test/example_project/deployment_config.py
+++
@@ -1,8 +0,0 @@
-from bigflow.configuration import Config
-
-deployment_config = Config(name='dev',
-                           properties={
-                               'docker_repository': 'test_repository',
-                               'gcp_project_id': '{project}',
-                               'dags_bucket': '{dags_bucket}'
-                           }).add_configuration()
