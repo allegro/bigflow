202e0f6fec9ca1ac9f33f88442b08a1959e0708c
Bartlomiej Beczkowski <beczkowb@gmail.com>
2020-08-27 14:44:00 +0200

    bumping to dev20

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open(os.path.join('requirements', 'bigquery_extras.txt'), 'r') as bigquery_
 
 setuptools.setup(
     name="bigflow",
-    version="1.0.dev19",
+    version="1.0.dev20",
     author=u"Chi",
     author_email="chibox-team@allegrogroup.com",
     description="BigQuery client wrapper with clean API",
