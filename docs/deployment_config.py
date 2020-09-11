a435e4e6d3a205dcabc8bd01a5b64dc7981cd93c
Bartosz Walacik <bartosz.walacik@allegro.pl>
2020-09-11 09:22:56 +0200

    tutorial, case fix

--- docs/deployment_config.py
+++ docs/deployment_config.py
@@ -3,7 +3,7 @@ from bigflow import Config
 deployment_config = Config(
     name='dev',
     properties={
-        'gcp_project_id': 'MY_GCP_PROJECT_ID',
+        'gcp_project_id': 'my_gcp_project_id',
         'docker_repository': 'eu.gcr.io/{gcp_project_id}/docs-project',
-        'dags_bucket': 'MY_COMPOSER_DAGS_BUCKET'
+        'dags_bucket': 'my_composer_dags_bucket'
     })
