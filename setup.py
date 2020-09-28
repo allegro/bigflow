2123b8df585534d9e7b9c6b9c8bf9b9ed8563e66
Iwo Czerniawski <iwo.czerniawski@allegro.pl>
2020-09-28 12:30:16 +0200

    add logging to stackdriver

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open(os.path.join('requirements', 'bigquery_extras.txt'), 'r') as bigquery_
 
 setuptools.setup(
     name="bigflow",
-    version="1.0.dev43",
+    version="1.0.dev44",
     author=u"Chi",
     author_email="chibox-team@allegrogroup.com",
     description="BigQuery client wrapper with clean API",
