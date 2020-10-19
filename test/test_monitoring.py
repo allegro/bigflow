486f5745a9b7a6373c4134d72b8bfbcb8a7d48db
Andrei Zhlobich <andrei.zhlobich@allegro.pl>
2020-10-19 18:08:34 +0200

    Fix monitoring patcher / test

--- test/test_monitoring.py
+++ test/test_monitoring.py
@@ -1,6 +1,7 @@
 from unittest import TestCase, mock
 from datetime import datetime
 
+import bigflow
 from bigflow import monitoring
 from freezegun import freeze_time
 
@@ -221,7 +222,7 @@ class MeterJobRunFailuresTestCase(TestCase):
 
         # when
         with self.assertRaises(Exception):
-            metered_job.run('2019-01-01')
+            metered_job.execute(bigflow.JobContext.make(runtime='2019-01-01'))
 
         # then
         increment_job_failure_count_mock.assert_called_once_with(monitoring_config, 'job1')
