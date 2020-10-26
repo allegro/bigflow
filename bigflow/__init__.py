b89705a701d5f957c192d6ce41dc7afdd5c69536
Andrei Zhlobich <andrei.zhlobich@allegro.pl>
2020-10-26 13:33:15 +0100

    Don't add submodules to __init__/__all__
    Magic module attribute `__all__` is used only during `star import`.
    Usually it is not recommended to use, also it is not mentioned in docs.

--- bigflow/__init__.py
+++ bigflow/__init__.py
@@ -21,13 +21,11 @@ __all__ = [
 
 try:
     from . import bigquery
-    __all__.append('bigquery')
 except ImportError:
     pass
 
 try:
     from . import monitoring
-    __all__.append('monitoring')
 except ImportError:
     pass
 
