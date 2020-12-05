b66f94afa16cc831eec8b704911ad7f543d38962
Andrei Zhlobich <andrei.zhlobich@allegro.pl>
2020-12-05 18:16:02 +0100

    Add simple projets for tests

---
+++ test/bf-projects/bf_simple_v11/setup.py
@@ -0,0 +1,8 @@
+import bigflow.build
+
+bigflow.build.setup(
+    name="bf_simple_v11",
+    author="Schrödinger's cat",
+    descriptino="Sample Bgiflow project for unit tests",
+    url="http://example.org",
+)
