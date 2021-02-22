cd99c44a14ea666637d5f5843563769dc3b39a41
Andrei Zhlobich <andrei.zhlobich@allegro.pl>
2021-02-22 14:04:02 +0100

    Remove old workaround #185

--- bigflow/bigquery/__init__.py
+++ bigflow/bigquery/__init__.py
@@ -31,16 +31,3 @@ def sensor(table_alias, where_clause, ds=None): ...
 
 @public(alias_for=interactive.interactive_component)
 def component(**dependencies): ...
-
-
-import logging
-logger = logging.getLogger(__name__)
-
-try:
-    # To avoid using pyarrow in the to_dataframe method. Beam requires obsolete version of pyarrow, which is missing a feature
-    # required by the google-cloud-bigquery
-    # in exception clause because of https://github.com/allegro/bigflow/issues/149
-    from google.cloud.bigquery import table
-    table.pyarrow = None
-except Exception as e:
-    logger.warning("Can't import google.cloud.bigquery or google.cloud.bigquery.table.pyarrow property not found.")
