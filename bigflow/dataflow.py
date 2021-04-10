f5f9c07bad241d4c893a1348e3252d2e02ab4b3a
Andrei Zhlobich <andrei.zhlobich@allegro.pl>
2021-04-10 17:44:11 +0200

    BeamJob - dict as pipeline_options, fill defaults

--- bigflow/dataflow.py
+++ bigflow/dataflow.py
@@ -1,12 +1,14 @@
 import typing
 import logging
+import uuid
 
 from apache_beam import Pipeline
-from apache_beam.options.pipeline_options import PipelineOptions, GoogleCloudOptions
+from apache_beam.options.pipeline_options import PipelineOptions
+
+from typing import Dict, Union
 
 from bigflow.commons import public
 from bigflow.workflow import Job, JobContext
-from bigflow.workflow import DEFAULT_EXECUTION_TIMEOUT_IN_SECONDS, DEFAULT_PIPELINE_LEVEL_EXECUTION_TIMEOUT_SHIFT_IN_SECONDS
 
 logger = logging.getLogger(__file__)
 
@@ -14,41 +16,58 @@ logger = logging.getLogger(__file__)
 @public()
 class BeamJob(Job):
 
+    pipeline_level_execution_timeout_shift = 120  # 2 minutes
+
     def __init__(
             self,
-            id: str,
-            entry_point: typing.Callable[[Pipeline, JobContext, dict], None],
-            pipeline_options: PipelineOptions = None,
+            id: str = None,
+            entry_point: typing.Callable[[Pipeline, JobContext, dict], None] = None,
+            pipeline_options: Union[typing.Dict, PipelineOptions, None] = None,
             entry_point_arguments: typing.Optional[dict] = None,
             wait_until_finish: bool = True,
-            execution_timeout_sec: int = DEFAULT_EXECUTION_TIMEOUT_IN_SECONDS,
             test_pipeline: Pipeline = None,
+            execution_timeout_sec: int = None,
+            **kwargs,
     ):
         if bool(test_pipeline) == bool(pipeline_options):
             raise ValueError("One of the pipeline and pipeline_options must be provided.")
+
         if not wait_until_finish and execution_timeout_sec:
             raise ValueError("If wait_until_finish_set to False execution_timeout can not be used.")
 
-        self.id = id
+        super().__init__(
+            id=id,
+            execution_timeout_sec=execution_timeout_sec,
+        )
+
+        if isinstance(pipeline_options, PipelineOptions):
+            pipeline_options = pipeline_options.get_all_options(drop_default=True)
+            self.add_default_pipeline_options = False
+        else:
+            self.add_default_pipeline_options = True
+
+        self.pipeline_options = dict(pipeline_options or {})
+        assert PipelineOptions.from_dictionary(self.pipeline_options)
+
         self.entry_point = entry_point
         self.entry_point_arguments = entry_point_arguments
-        self.pipeline_options = pipeline_options
         self.wait_until_finish = wait_until_finish
-        self.pipeline = test_pipeline
-        self.execution_timeout_sec = execution_timeout_sec
-        self.pipeline_level_execution_timeout_shift = DEFAULT_PIPELINE_LEVEL_EXECUTION_TIMEOUT_SHIFT_IN_SECONDS
+
+        self._test_pipeline = test_pipeline
 
     def execute(self, context: JobContext):
-        if self.pipeline:
-            pipeline = self.pipeline
-        else:
-            if context.workflow:
-                self.pipeline_options = self._apply_logging(self.pipeline_options, context.workflow.workflow_id)
-            else:
-                logger.info("A workflow not found in the context. Skipping logging initialization.")
-            pipeline = self._create_pipeline(self.pipeline_options)
-        self.entry_point(pipeline, context, self.entry_point_arguments)
-        result = pipeline.run()
+        pipeline = self._test_pipeline or self.create_pipeline(context)
+
+        logger.info("init beam pipeline...")
+        self.init_pipeline(context, pipeline)
+
+        logger.info("run beam pipeline...")
+        result = self.run_pipeline(context, pipeline)
+
+        logger.info("wait pipeline result...")
+        self.wait_pipeline_result(result)
+
+    def wait_pipeline_result(self, result: Pipeline):
         if self.wait_until_finish and self.execution_timeout_sec:
             timeout_in_milliseconds = 1000 * (self.execution_timeout_sec - self.pipeline_level_execution_timeout_shift)
             result.wait_until_finish(timeout_in_milliseconds)
@@ -56,14 +75,46 @@ class BeamJob(Job):
                 result.cancel()
                 raise RuntimeError(f'Job {self.id} timed out ({self.execution_timeout_sec})')
 
-    def _create_pipeline(self, options):
-        return Pipeline(options=options)
+    def init_pipeline(self, context, pipeline):
+        if not self.entry_point:
+            raise RuntimeError("You need override method 'init_pipeline' or provide 'entry_point' argument")
+        self.entry_point(pipeline, context, self.entry_point_arguments)
 
-    @staticmethod
-    def _apply_logging(pipeline_options: PipelineOptions, workflow_id: str) -> PipelineOptions:
-        google_cloud_options = pipeline_options.view_as(GoogleCloudOptions)
-        if not google_cloud_options.labels:
-            google_cloud_options.labels = []
-        google_cloud_options.labels.append(f'workflow_id={workflow_id}')
-        return pipeline_options
+    def run_pipeline(self, context, pipeline):
+        logger.info("Run pipeline, context %s", context)
+        return pipeline.run()
+
+    def create_pipeline(self, context):
+        logger.debug("Create new pipline for context %s", context)
+        popts = self.create_pipeline_options(context)
+        return Pipeline(options=popts)
+
+    def create_pipeline_options(self, context: JobContext) -> PipelineOptions:
+        options = dict(self.pipeline_options)
+        if self.add_default_pipeline_options:
+            self.set_default_pipeline_options(options, context)
+        logger.debug("Pipeline options from dict %s", options)
+        return PipelineOptions.from_dictionary(options)
+
+    def set_default_pipeline_options(self, options, context):
+        logger.debug("Add defaults to pipeline options")
+
+        if 'job_name' not in options:
+            job_name = f"{self.id}-{uuid.uuid4().hex}"
+            logger.info("Set job name to deafult %s", job_name)
+            options['job_name'] = job_name
+
+        if 'setup_file' not in options:
+            from bigflow.build.reflect import materialize_setuppy
+            setuppy = materialize_setuppy()
+            logging.debug("Add setup.py %s to pipeline options", setuppy)
+            options['setup_file'] = materialize_setuppy()
+        else:
+            logging.debug("Pipeline options already contains 'setup_file': %s", options['setup_file'])
+
+        workflow_id = context.workflow_id
+        if workflow_id:
+            options['labels'] = (options.get('labels') or []) + [f'workflow_id={workflow_id}']
+        else:
+            logger.info("A workflow not found in the context. Skipping logging initialization.")
 
