83f32f5b39bbf9175aea0ad637e6fe440b633ccb
Bartlomiej Beczkowski <beczkowb@gmail.com>
2021-06-20 14:46:03 +0200

    bigflow.deploy type hints, refactoring

--- bigflow/deploy.py
+++ bigflow/deploy.py
@@ -1,11 +1,10 @@
 import logging
+import typing as T
 from pathlib import Path
 
+import requests
 from google.cloud.storage import Bucket
 from google.oauth2 import credentials
-from subprocess import run, CompletedProcess
-import requests
-from typing import List
 from google.cloud import storage
 
 import bigflow.commons as bf_commons
@@ -23,16 +22,17 @@ def load_image_from_tar(image_tar_path: str) -> str:
     raise ValueError(f"Can't load image: {image_tar_path}")
 
 
-def tag_image(image_id, repository, tag):
+def tag_image(image_id: str, repository: str, tag: str) -> str:
     return bf_commons.run_process(["docker", "tag", image_id,  f"{repository}:{tag}"])
 
 
 def deploy_docker_image(
         image_tar_path: str,
         docker_repository: str,
-        auth_method: str = 'local_account',
-        vault_endpoint: str = None,
-        vault_secret: str = None):
+        auth_method: T.Literal['local_account', 'vault'] = 'local_account',
+        vault_endpoint: T.Optional[str] = None,
+        vault_secret: T.Optional[str] = None,
+) -> str:
     build_ver = decode_version_number_from_file_name(Path(image_tar_path))
     build_ver = build_ver.replace("+", "-")  # fix local version separator
     image_id = load_image_from_tar(image_tar_path)
@@ -44,13 +44,13 @@ def deploy_docker_image(
 
 
 def _deploy_image_loaded_to_local_registry(
-    build_ver,
-    docker_repository,
-    image_id,
-    auth_method,
-    vault_endpoint,
-    vault_secret,
-):
+    build_ver: str,
+    docker_repository: str,
+    image_id: str,
+    auth_method: T.Literal['local_account', 'vault'],
+    vault_endpoint: str,
+    vault_secret: str,
+) -> str:
     tag_image(image_id, docker_repository, build_ver)
 
     docker_image = docker_repository + ":" + build_ver
@@ -74,11 +74,17 @@ def _deploy_image_loaded_to_local_registry(
     return docker_image
 
 
-def deploy_dags_folder(dags_dir: str, dags_bucket: str, project_id: str, clear_dags_folder: bool = False,
-                       auth_method: str = 'local_account', vault_endpoint: str = None, vault_secret: str = None,
-                       gs_client=None):
-
-    print(f"Deploying DAGs folder, auth_method={auth_method}, clear_dags_folder={clear_dags_folder}, dags_dir={dags_dir}")
+def deploy_dags_folder(
+        dags_dir: str,
+        dags_bucket: str,
+        project_id: str,
+        clear_dags_folder: bool = False,
+        auth_method: str = 'local_account',
+        vault_endpoint: T.Optional[str] = None,
+        vault_secret: T.Optional[str] = None,
+        gs_client: T.Optional[storage.Client] = None,
+) -> str:
+    logger.info(f"Deploying DAGs folder, auth_method={auth_method}, clear_dags_folder={clear_dags_folder}, dags_dir={dags_dir}")
 
     client = gs_client or create_storage_client(auth_method, project_id, vault_endpoint, vault_secret)
     bucket = client.bucket(dags_bucket)
@@ -90,37 +96,42 @@ def deploy_dags_folder(dags_dir: str, dags_bucket: str, project_id: str, clear_d
     return dags_bucket
 
 
-def clear_remote_dags_bucket(bucket: Bucket):
+def clear_remote_dags_bucket(bucket: Bucket) -> None:
     i = 0
     for blob in bucket.list_blobs(prefix='dags'):
         if not blob.name in ['dags/', 'dags/airflow_monitoring.py']:
-            print(f"deleting file {_blob_uri(blob)}")
+            logger.info(f"deleting file {_blob_uri(blob)}")
             blob.delete()
             i += 1
 
-    print(f"{i} files deleted")
+    logger.info(f"{i} files deleted")
 
 
-def _blob_uri(blob):
+def _blob_uri(blob: storage.Blob) -> str:
     return f"gs://{blob.bucket.name}/{blob.name}"
 
 
-def upload_dags_folder(dags_dir: str, bucket: Bucket):
+def upload_dags_folder(dags_dir: str, bucket: Bucket) -> None:
     dags_dir_path = Path(dags_dir)
 
     def upload_file(local_file_path, target_file_name):
         blob = bucket.blob(target_file_name)
         blob.upload_from_filename(local_file_path, content_type='application/octet-stream')
-        print(f"uploading file {local_file_path} to {_blob_uri(blob)}")
+        logger.info(f"uploading file {local_file_path} to {_blob_uri(blob)}")
 
     files = list(filter(Path.is_file, dags_dir_path.iterdir()))
     for f in files:
         upload_file(f.as_posix(), 'dags/' + f.name)
 
-    print(f"{len(files)} files uploaded")
+    logger.info(f"{len(files)} files uploaded")
 
 
-def create_storage_client(auth_method: str, project_id: str, vault_endpoint: str, vault_secret: str):
+def create_storage_client(
+        auth_method: str,
+        project_id: str,
+        vault_endpoint: str,
+        vault_secret: str,
+) -> storage.Client:
     if auth_method == 'local_account':
         return storage.Client(project=project_id)
     elif auth_method == 'vault':
@@ -130,7 +141,7 @@ def create_storage_client(auth_method: str, project_id: str, vault_endpoint: str
         raise ValueError('unsupported auth_method: ' + auth_method)
 
 
-def get_vault_token(vault_endpoint: str, vault_secret: str):
+def get_vault_token(vault_endpoint: str, vault_secret: str) -> str:
     if not vault_endpoint:
         raise ValueError('vault_endpoint is required')
     if not vault_secret:
@@ -140,10 +151,10 @@ def get_vault_token(vault_endpoint: str, vault_secret: str):
     response = requests.get(vault_endpoint, headers=headers, verify=False)
 
     if response.status_code != 200:
-        print(response.text)
+        logger.info(response.text)
         raise ValueError(
             'Could not get vault token, response code: {}'.format(
                 response.status_code))
 
-    print(f"get oauth token from {vault_endpoint} status_code={response.status_code}")
+    logger.info(f"get oauth token from {vault_endpoint} status_code={response.status_code}")
     return response.json()['data']['token']
