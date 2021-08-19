b54e2869a7d81a2cf1309db34e092a9291420aa3
Iwo Czerniawski <iwo.czerniawski@allegro.pl>
2021-08-19 09:09:45 +0200

    Change get_partition_from_run_datetime_or_none method to work with hourly partitions

--- bigflow/bigquery/dataset_manager.py
+++ bigflow/bigquery/dataset_manager.py
@@ -40,8 +40,9 @@ def handle_key_error(method):
 
 def get_partition_from_run_datetime_or_none(run_datetime):
     """
-    :param run_datetime: string run datetime in format YYYY-MM-DD HH:mm:ss or YYY-MM-DD
-    :return: string partition in format YYYYMMDD
+    :param run_datetime: string run datetime in format YYYY-MM-DD HH:mm:ss or YYYY-MM-DD
+    :return: string partition in format YYYYMMDDHH for run_datetime in format: YYYY-MM-DD HH:mm:ss or
+     YYYYMMDD for run_datetime in format YYYY-MM-DD
     """
     if run_datetime is not None:
         return run_datetime.replace('-', '').replace(':', '').replace(' ', '')[:10]
