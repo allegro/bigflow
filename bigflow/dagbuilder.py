4d51977c299df4031cddabb442067c0bbe3402c2
karol.stula <karol.stula@allegro.pl>
2023-04-19 23:07:10 +0200

    Fix troubles with composer in version 2. Get airflow version and then distinguish code

--- bigflow/dagbuilder.py
+++ bigflow/dagbuilder.py
@@ -68,7 +68,7 @@ def generate_dag_file(
             
         # BigFlow assumes that you use (airflow 1.x + composer 1.x) or (airflow 2.x + composer 2.x)
         IS_COMPOSER_2_X = version.version >= '2.0.0'
-        IS_AIRFLOW_2_3_X = airflow.__version__ >= '2.3.0'
+        IS_AIRFLOW_2_3_X = version.version >= '2.3.0'
         namespace = 'composer-user-workloads' if IS_COMPOSER_2_X else 'default'
             
         default_args = dict(
