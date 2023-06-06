a09970bdd26cd6f1df1d4a829f3e1c857f94cbd7
Alicja Nowak <alicja.nowak@allegro.pl>
2023-06-06 09:55:08 +0200

    FST-819 | logging messages - quick check

--- bigflow/bigquery/dataset_manager.py
+++ bigflow/bigquery/dataset_manager.py
@@ -450,12 +450,12 @@ def create_bigquery_client(
 ) -> 'google.cloud.bigquery.Client':
     from google.cloud import bigquery
     logger.info("Credentials logs - create_bigquery_client", bigquery.Client(
-        project=project_id,
+        project="sc-14793-bigflow-prod",
         credentials=credentials,
         location=location).__dict__)
 
     return bigquery.Client(
-        project=project_id,
+        project="sc-14793-bigflow-prod",
         credentials=credentials,
         location=location)
 
